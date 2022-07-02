78221b9b45d6b885bd2012399e8f7e0daf61ce38
Thinh Nguyen <thanhthinhcntt@gmail.com>
2022-07-02 19:20:26 +0700

    add

---
+++ shoplaravel/app/Http/Controllers/LoginController.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Session;
+use Illuminate\Support\Facades\Validator;
+
+class LoginController extends Controller
+{
+    public function login (Request $request)
+    {
+        $rules = ['email' => 'required|email', 'password' => 'required|min:6'];
+        $messages = [
+            'email.required' => 'Email không được phép để trống!',
+            'email.email' => 'Email không đúng định dạng!',
+            'password.required' => 'Password không được phép để trống!',
+            'password.min' => 'Password phải chứa ít nhất 6 kí tự!'
+        ];
+
+        $validator = Validator::make($request->all(), $rules, $messages);
+
+        if ($validator->fails()) {
+            return redirect()->back()->withErrors($validator)->withInput();
+        } else {
+            $email = $request->input('email');
+            $password = $request->input('password');
+            if (Auth::attempt(['email' => $email, 'password' => $password])) {
+                return redirect()->route('home');
+            } else {
+                Session::flash('error', 'Bạn đăng nhập sai tải khoản hoặc mật khẩu!');
+                return redirect()->back();
+            }
+        }
+    }
+}
---
+++ shoplaravel/resources/views/login.blade.php
@@ -0,0 +1,51 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Login</title>
+    <link rel='stylesheet' href='https://fonts.googleapis.com/css?family=Open+Sans:600'>
+    <link rel="stylesheet" href="{{ asset('css/login_style.css') }}">
+</head>
+<body>
+<div class="login-wrap">
+    <div class="login-html">
+        @if (Session::has('error'))
+            <p class="text-danger" style="color: red">
+                <i class="fas fa-exclamation-triangle" aria-hidden="true"></i>
+                {{ Session::get('error') }}
+            </p>
+        @endif
+        <input id="tab-1" type="radio" name="tab" class="sign-in" checked><label for="tab-1" class="tab">Đăng nhập</label>
+        <input id="tab-2" type="radio" name="tab" class="sign-up"><label for="tab-2" class="tab"></label>
+        <div class="login-form">
+            <div class="sign-in-htm">
+                <form method="post" action="/">
+                    @csrf
+                    <div class="group">
+                        <label for="email" class="label">Email</label>
+                        <input id="email" type="text" name="email" class="input" value="{{ old('email') }}">
+                    </div>
+                    @if($errors->has('email'))
+                        <p class="text-danger" style="color: red">{{ $errors->first('email') }}</p>
+                    @endif
+                    <div class="group">
+                        <label for="password" class="label">Mật khẩu</label>
+                        <input id="password" type="password" name="password" class="input" data-type="password">
+                    </div>
+                    @if($errors->has('password'))
+                        <p class="text-danger" style="color: red">{{ $errors->first('password') }}</p>
+                    @endif
+                    <div class="group">
+                        <input id="check" type="checkbox" class="check" checked>
+                        <label for="check"><span class="icon"></span> Ghi nhớ tài khoản</label>
+                    </div>
+                    <div class="group">
+                        <input type="submit" class="button" value="Đăng nhập">
+                    </div>
+                </form>
+            </div>
+        </div>
+    </div>
+</div>
+</body>
+</html>
