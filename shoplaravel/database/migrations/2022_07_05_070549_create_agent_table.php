a367ecff7eae0c6c228cb5c489ece91335015be7
Thinh Nguyen <ntthinh@cusc.ctu.edu.vn>
2022-07-05 19:21:33 +0900

    up

---
+++ shoplaravel/database/migrations/2022_07_05_070549_create_agent_table.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('agent', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('email')->unique();
+            $table->string('address');
+            $table->string('phone');
+            $table->string('bank_code');
+            $table->string('branch_code');
+            $table->string('normal');
+            $table->string('account_no')->unique();
+            $table->string('curator');
+            $table->string('line_url')->unique();
+            $table ->decimal('margin_rate');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('agent');
+    }
+};
