98f04e94239509f476241b187823cadb76b36bdc
Thinh Nguyen <ntthinh@cusc.ctu.edu.vn>
2022-07-05 11:50:11 +0900

    update router

--- shoplaravel/routes/web.php
+++ shoplaravel/routes/web.php
@@ -4,6 +4,7 @@ use Illuminate\Support\Facades\Route;
 use \App\Http\Controllers\LoginController;
 use \App\Http\Controllers\HomeController;
 use \App\Http\Controllers\LogoutController;
+use \App\Http\Controllers\AgentController;
 
 /*
 |--------------------------------------------------------------------------
@@ -28,4 +29,17 @@ Route::get('/logout',[LogoutController::class,'getLogout'])->name('logout');
 Route::group(['middleware' => 'auth', 'timeout'], function (){
     Route::get('/home',[HomeController::class,'index'])->name('home');
 
+    Route::get('/agents',[AgentController::class,'index'])->name('agent_index');
+
+    Route::get('/create-agent',[AgentController::class,'create'])->name('agent_create');
+
+    Route::post('/add-agent',[AgentController::class,'store'])->name('agent_store');
+
+    Route::get('/edit-agent/{id}',[AgentController::class,'edit'])->name('agent_edit');
+
+    Route::post('/update-agent/{id}',[AgentController::class,'update'])->name('agent_update');
+
+    Route::get('/delete-agent/{id}',[AgentController::class,'destroy'])->name('agent_destroy');
+
 });
+
