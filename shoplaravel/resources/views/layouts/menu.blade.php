6d22ba589492ac2a5a0dec97617bed7cece07b60
Thinh Nguyen <ntthinh@cusc.ctu.edu.vn>
2022-07-07 11:08:32 +0900

    add menu

--- shoplaravel/resources/views/layouts/menu.blade.php
+++ shoplaravel/resources/views/layouts/menu.blade.php
@@ -2,7 +2,7 @@
     <li>
         <a href="javascript: void(0);"> <i data-feather="home" class="align-self-center menu-icon"></i><span>Trang chủ</span><span class="menu-arrow"><i class="mdi mdi-chevron-right"></i></span></a>
         <ul class="nav-second-level" aria-expanded="false">
-            <li class="nav-item"><a class="nav-link" href="#"><i class="ti-control-record"></i>Menu 1</a></li>
+            <li class="nav-item"><a class="nav-link" href="{{route('agent_index')}}"><i class="ti-control-record"></i>代理店登録</a></li>
         </ul>
     </li>
 </ul>
